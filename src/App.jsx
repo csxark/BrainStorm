e167cfda43697018a6d37ce4d8c52a066ed1c8b0
csxark <arktandoncs@gmail.com>
2025-02-18 01:03:02 +0530

    navbar updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Features from './Components/Features'
@@ -7,20 +7,28 @@ import Pricing from './Components/Pricing'
 import Testimonals from './Components/Testimonals'
 import Footer from './Components/Footer'
 
-
 function App() {
-
   return (
     <>
-    <Navbar />
-    <div className="max-w-7xl mx-auto pt-20 px-6">
-    <Hero/>
-    <Features/>
-    <Workflow />
-    <Pricing/>
-    <Testimonals />
-    </div>
-    <Footer/>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Features />
+              <Workflow />
+              <Pricing />
+              <Testimonals />
+            </>
+          } />
+          <Route path="/features" element={<Features />} />
+          <Route path="/workflow" element={<Workflow />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/testimonials" element={<Testimonals />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
   )
 }
